848e70d69e9556f47ab8729394f8d84fa2d945ae
sellohay <sskrater@yandex.ru>
2026-02-13 01:08:25 +0300

    исправление checkstyle

--- src/main/ru/practicum/moviehub/MovieHubApp.java
+++ src/main/ru/practicum/moviehub/MovieHubApp.java
@@ -3,7 +3,6 @@ package ru.practicum.moviehub;
 import ru.practicum.moviehub.http.MoviesServer;
 import ru.practicum.moviehub.store.MoviesStore;
 
-import java.io.IOException;
 
 public class MovieHubApp {
     public static void main(String[] args) {
